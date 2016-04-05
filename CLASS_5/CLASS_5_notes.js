eef27aa013f8478aab1227112aa768f372628094
Sara E <eichnersara@gmail.com>
2016-04-05 10:31:02 -0400

    in progress
    update to desktop

---
+++ CLASS_5/CLASS_5_notes.js
@@ -0,0 +1,47 @@
+
+HTTP template strying for 
+
+function {name} ({parameters}) {
+	{code}
+}
+
+
+	])
+
+
+outside of calss
+
+work on updated api sql for homework
+
+- go over class JS.bins Carefully
+
+- class exercise 1 from class 5
+
+finding data where you can not donwload it
+
+use console, network, 
+refresh
+select XHR 
+dig through data
+
+you can right click on data fileds or copy and paste and take all the data
+you can also see what has been searched 
+- this can help for debugging
+
+- look at mapbrief tm 
+good blog to check out
+static maps available on 
+cartodb
+mapbox
+and a few other places
+
+good for mobile situations
+
+styling in leaflet
+under path options 
+
+- look at leaflet stying info from class
+how to make style object functions
+L.geoJson(data, {
+	style: function (feature)
+})
