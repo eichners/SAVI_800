70b31660ffd21bb8e0e91a1e1828cb02e7737e25
eichners <eichnersara@gmail.com>
2016-04-06 17:55:34 -0400

    new attempts at assignment 2
    none working yet

--- Assignment_2/script.js
+++ Assignment_2/script.js
@@ -9,59 +9,59 @@ $(document).ready(function () {
 	   attribution: 'Map tiles by <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 	    maxZoom: 18
 	}).addTo(map);
-	 var oddLotsData;
+	
   
   // add sql data from cartodb odd lots file	
+  	var oddLotsData;
 	var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
 	    q: 'SELECT * FROM bk_oddlots ORDER BY address DESC LIMIT 500',
 	    format: 'GeoJSON'
 	});
-	     $.getJSON(url)
-	     .done(function (data) {
-	     var oddlotsLayer = data
-	     var lotStyle = {color: '#ff4000',
-	     				fillColor : "#ff4000",
-	         			 weight: 3,
-        				 opacity: 0.9
-         };
+	$.getJSON(url)
+	.done(function (data) {
+	var oddlotsLayer = data
+	var lotStyle = {
+	    color: '#ff4000',
+	    fillColor : "#ff4000",
+	    weight: 3,
+        opacity: 0.9
+    };
 
-		    var lotClick = function (feature, layer) {
-			layer.bindPopup("<strong>" + feature.properties.address + "</strong>" + "," + "<br/>" + feature.properties.ownername );
-		    }
-		    
-		    oddLotsData = L.geoJson(oddlotsLayer, {
-		    	style: lotStyle,
-		    	onEachFeature: lotClick
-		    }).addTo(map); 
-	      
-	      map.fitBounds(oddLotsData.getBounds());
-// double quotes in sql means column!!
-// single quotes are for strings so all text is single quotes
-	$('.choice').change(function () {
-	var sql = 'SELECT * FROM bk_oddlots';
-	if ($(this).val() === '*') {
+	var lotClick = function (feature, layer) {
+	layer.bindPopup("<strong>" + feature.properties.address + "</strong>" + "," + "<br/>" + feature.properties.ownername );
 	}
-	else if ($(this).val() === 'null'){}
-	else { 
-		sql = "WHERE ownertype = '" +  ($(this.val()) + "'"	
-			}
+		    
+		oddLotsData = L.geoJson(oddlotsLayer, {
+			  style: lotStyle,
+			  onEachFeature: lotClick
+		}).addTo(map); 
+	      
+	map.fitBounds(oddLotsData.getBounds());
+		// double quotes in sql means column single quotes are for strings so all text is single quotes
 	
-    var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
-      q: sql,
-      format: 'GeoJSON'
-    });
-    $.getJSON(url)
+	$('.choice').change(function () {
+		var sql = 'SELECT * FROM bk_oddlots';
+		if ($(this).val() === 'ownertype') {
+		}
+		else if ($(this).val() === 'null'){	
+		}
+		else { 
+		sql = "WHERE ownertype = '" +  ($(this).val()) + "'";	
+		}
+	
+	    var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
+		    q: sql,
+		    format: 'GeoJSON'
+	    });
+	    $.getJSON(url)
 
 // error with data loading above with anonymous function
-      .done(function (data) {
-      	console.log(data);
-      	var oddlotsLayer = data
- 
-        oddLotsData.clearLayers();
-        oddLotsData.addData(data);
-
-
-	  });
+	     .done(function (data) {
+	      	console.log(data);
+	//      var oddlotsLayer = data
+	        oddLotsData.clearLayers();
+	        oddLotsData.addData(data);
+		 });
+	});
 	});
 });
-	     });
