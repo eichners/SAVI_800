4879193defaeef398c8b75137e384c2d29f9e021
Sara E <eichnersara@gmail.com>
2016-04-20 21:47:11 -0400

    class 10 notes

---
+++ CLASS_10/CLASS_10_notes.js
@@ -0,0 +1,30 @@
+MAP BOX 
+
+BASE TILES
+
+images, 
+
+each tile has 3 coordinates: 
+Zoom, X, Y
+
+tileMill
+
+data, style with cartocss, create tiles, then upload to a server -- whatever one you want. 
+
+
+using mapbox with cartodb, go to add custom basemap and copy url and 
+DO NOT click on mapbox to enter, use the x,y, z button
+
+uploading data to mapbox
+go to Tilesets
+in account under tilesets on left toolbar
+
+up to 1 gigabyte of memory storage available
+
+
+use pluto etc 
+with new data, click new layer
+source click no tile set
+scroll down and find my source
+then you'll see the source
+then style and filter the data to style the mapmake new layers with each filter 
